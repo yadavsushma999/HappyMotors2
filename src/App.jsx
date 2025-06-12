ace141bd170d3b856c44d9e5062867a11aa315b6
swaraj-tech <swarajtech999@gmail.com>
2025-06-12 15:32:18 +0530

    Some bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { ThemeProvider } from './components/ThemeContext';
 import ScrollToTop from './components/ScrollToTop'; // ⬅️ Import ScrollToTop
 import HomePage from './pages/HomePage';
